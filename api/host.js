2e293be1dc257f8aadce349f41d512394ffe1fb7
Shrey Gupta <shreysgupta@gmail.com>
2020-11-08 15:59:47 +0000

    asdf

--- api/host.js
+++ api/host.js
@@ -9,19 +9,25 @@ module.exports = async (requesting_data, responding_data) => {
     console.log("name", name)
     console.log("base", base)
 
-    var options = {
-      method: 'POST',
-      url: 'https://api.vercel.com/v12/now/deployments',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer nTTzOm6rofsvMr8w5TzoCEYn'
-      },
-      data: {
-        name: 'my-instant-deployment',
-        files: [{file: name, data: base, encoding: 'base64'}],
-        projectSettings: {framework: null}
+var options = {
+  method: 'POST',
+  url: 'https://api.vercel.com/v12/now/deployments',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer nTTzOm6rofsvMr8w5TzoCEYn'
+  },
+  data: {
+    name: 'my-instant-deployment',
+    files: [
+      {
+        file: 'index.png',
+        data: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg==',
+        encoding: 'base64'
       }
-    };
+    ],
+    projectSettings: {framework: null}
+  }
+};
 
     axios.request(options).then(function(response) {
       console.log(response.data);
