6ad0eb827e2300b48f2c686102dc4ed15bb12bfc
Shrey Gupta <shreysgupta@gmail.com>
2020-11-08 15:50:25 +0000

    csaxios

--- api/host.js
+++ api/host.js
@@ -1,4 +1,4 @@
-const http = require("https");
+var axios = require("axios").default;
 
 module.exports = async (requesting_data, responding_data) => {
 
@@ -9,46 +9,26 @@ module.exports = async (requesting_data, responding_data) => {
     console.log("name", name)
     console.log("base", base)
 
-    const options = {
-      "method": "POST",
-      "hostname": "api.vercel.com",
-      "port": null,
-      "path": "/v12/now/deployments",
-      "headers": {
-        "Content-Type": "application/json",
-        "Content-Length": "211",
-        "Authorization": "Bearer nTTzOm6rofsvMr8w5TzoCEYn"
+    var options = {
+      method: 'POST',
+      url: 'https://api.vercel.com/v12/now/deployments',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer nTTzOm6rofsvMr8w5TzoCEYn'
+      },
+      data: {
+        name: 'my-instant-deployment',
+        files: [{file: name, data: base, encoding: 'base64'}],
+        projectSettings: {framework: null}
       }
     };
 
-    const req = await http.request(options, function (res) {
-      const chunks = [];
-
-      res.on("data", function (chunk) {
-        chunks.push(chunk);
-        console.log("chunk", chunk)
-
-      });
-
-      res.on("end", function () {
-        const body = Buffer.concat(chunks);
-                console.log("body2", body)
-
-        console.log(body.toString());
-        returning = "hi" + body.toString()
-      });
+    axios.request(options).then(async function(response) {
+      console.log(response.data);
+      await responding_data.status(200).end(response.data);
+    }).catch(function (error) {
+      console.error(error);
+      await responding_data.status(500).end(error);
     });
-
-    await req.write(JSON.stringify({
-      name: "functionslolol",
-      files: [{file: name, data: base, encoding: 'base64'}],
-      projectSettings: {framework: null}
-    }));
-        console.log("write")
-
-    await req.end();
-        console.log("end")
-
-    await responding_data.status(201).end(returning);
 
 };
